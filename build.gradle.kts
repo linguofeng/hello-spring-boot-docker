6b9f3d3e8e21da966ec7158e9ca8359dd22515be
linguofeng <linguofeng@msn.com>
2017-02-10 14:17:09 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,23 @@
 buildscript {
     val springBootVersion = "1.4.3.RELEASE"
-	val kotlinVersion = "1.0.6"
+    val kotlinVersion = "1.0.6"
     extra["kotlinVersion"] = kotlinVersion
     repositories {
         mavenCentral()
     }
     dependencies {
         classpath("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
-		classpath("org.jetbrains.kotlin:kotlin-noarg:$kotlinVersion")
-		classpath("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
-		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-noarg:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
+
 apply {
-	plugin("idea")
-	plugin("kotlin")
-	plugin("kotlin-spring")
-	plugin("org.springframework.boot")
+    plugin("idea")
+    plugin("kotlin")
+    plugin("kotlin-spring")
+    plugin("org.springframework.boot")
 }
 
 version = "0.0.1"
@@ -26,8 +27,8 @@ repositories {
 }
 
 configure<JavaPluginConvention> {
-	setSourceCompatibility(1.8)
-	setTargetCompatibility(1.8)
+    setSourceCompatibility(1.8)
+    setTargetCompatibility(1.8)
 }
 
 val kotlinVersion = extra["kotlinVersion"] as String
@@ -35,7 +36,7 @@ val kotlinVersion = extra["kotlinVersion"] as String
 dependencies {
     compile("org.springframework.boot:spring-boot-starter-web")
     compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-	compile("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-	compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.8.4")
+    compile("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.8.4")
     testCompile("org.springframework.boot:spring-boot-starter-test")
 }
--- src/main/kotlin/hello/Application.kt
+++ src/main/kotlin/hello/Application.kt
@@ -3,10 +3,8 @@ package hello
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@SpringBootApplication
-open class Application {
-}
+@SpringBootApplication class Application
 
 fun main(args: Array<String>) {
-	SpringApplication.run(Application::class.java, *args)
+    SpringApplication.run(Application::class.java, *args)
 }
